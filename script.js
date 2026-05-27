c8540a3ddf17a0ce20a13049c02550ac5b9e8dc4
MePython313 <prakt1guru@gmail.com>
2026-05-27 14:11:53 +0530

    Implement play function for rock-paper-scissors game

---
+++ script.js
@@ -0,0 +1,73 @@
+function play(playerMove) {
+
+    stats.games++;
+
+    stats[playerMove]++;
+
+    let aiMove = getAIMove();
+
+    // AI prediction tracking
+
+    if (
+        getFavoriteMove() === playerMove
+    ) {
+
+        stats.aiCorrect++;
+    }
+
+    let result = "";
+
+    // WIN CONDITIONS
+
+    if (playerMove === aiMove) {
+
+        result = "🤝 Draw!";
+    }
+
+    else if (
+
+        (playerMove === "rock" &&
+        aiMove === "scissors")
+
+        ||
+
+        (playerMove === "paper" &&
+        aiMove === "rock")
+
+        ||
+
+        (playerMove === "scissors" &&
+        aiMove === "paper")
+
+    ) {
+
+        result = "🎉 You Win!";
+    }
+
+    else {
+
+        result = "💀 AI Wins!";
+    }
+
+    document.getElementById("result")
+        .innerHTML = `
+
+        You picked:
+        <b>${playerMove}</b>
+
+        <br><br>
+
+        AI picked:
+        <b>${aiMove}</b>
+
+        <br><br>
+
+        ${result}
+    `;
+
+    saveStats();
+
+    updateUI();
+}
+
+updateUI();
