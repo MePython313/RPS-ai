6a2f36a6897daa190f81321af605450a1a2fdc60
MePython313 <prakt1guru@gmail.com>
2026-05-27 16:03:05 +0530

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,17 +1,17 @@
 let stats = JSON.parse(
     localStorage.getItem("rpsStats")
 ) || {
-
-    games: 0,
-
-    rock: 0,
-
-    paper: 0,
-
-    scissors: 0,
-
-    aiCorrect: 0
+    wins: 0,
+    losses: 0,
+    draws: 0
 };
+
+function saveStats() {
+    localStorage.setItem(
+        "rpsStats",
+        JSON.stringify(stats)
+    );
+}
 
 function saveStats() {
 
